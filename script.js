138c12d69f1f7f30ea2bcf3fd25e4dbedcbba65a
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-03-07 17:19:42 +0100

    add sound

--- script.js
+++ script.js
@@ -1,84 +1,151 @@
-console.log('test');
-
 //cibler bouton commencer 
 const start = document.getElementById('start');
-//ecouter l element bouton start
-start.addEventListener('click', ()=>{
-    console.log('clic clic bang-bang');
+//----------------------------------on-off sound-----------------------------------------------------------
 
-})
-
-//cibler input select 
-const select = document.getElementById('compteur'); 
-const compteur = select.value; 
-console.log(select);
-
-
-// "soritr toute les valeurs de select"
-for (const minuteur of select) {
-    console.log(minuteur);
-}
-//test du selector 
-select.addEventListener('selector', ()=>{
-
-if (compteur === 1){
-    console.log('test 1 ok');
-}
-})
-
-
-//---------------------------------------------------------------------------------------
-// timer
-const but = document.getElementById('start');
-
-let cps = 1;
-
-
-but.addEventListener('click', () => {
-    if (cps == 1) {
-        setInterval(timing, 1000);
-    }
-
-    viewFinder.classList.toggle('active');
-    counter.textContent = cps++;
-    showTime()
-})
-
-// Relancer le script
-
-// Boîte du timer
-const timerCadre = document.querySelector('.timer');
-// Titre qui affiche le nombre de secondes qu'il reste
-let timerTitle = document.createElement('h2');
-// Minuteur de 10 secondes
-let i = 50;
-timerTitle.textContent = i-- + ' sec';
-// Afficher le titre dans la boîte du timer
-timerCadre.appendChild(timerTitle);
-
-
-const timing = () => {
-    if (i > 0) {
-        timerTitle.textContent = i-- + ' sec';
-    } else {
-        timerTitle.textContent= 'End';
-    }
-}
-
-//---------------------------------------------------------------------------------------------
-
-// basculement bouton audio on - opff 
-
-const audioOn = document.getElementById('iconOn'); 
+// basculement bouton audio on - off 
+//variable du btn on 
+const audioOn = document.getElementById('iconOn');
 console.log(audioOn);
-const audioOff = document.getElementById('iconOff'); 
+//variable du btn off
+const audioOff = document.getElementById('iconOff');
 console.log(audioOff);
+// variable audio 
+const audio = document.getElementById('audioPlayer');
+//ecoute le btn on 
+audioOn.addEventListener('click', () => {
+    //faire disparaitre le logo on 
+    audioOn.classList.add('hidden');
+    //fait apparaitre le logo off 
+    audioOff.classList.remove('hidden');
+    //lancer la piste audio 
+    audio.play(); 
+})
+//ecoute le bouton off
+audioOff.addEventListener('click', () => {
+    //fait apparaitre le logo on 
+    audioOff.classList.add('hidden');
+    //faire disparaitre le logo off 
+    audioOn.classList.remove('hidden');
+    //mettre l audio en pause
+    audio.pause();
+    //remise à zero de l audio 
+    // audio.currentTime = 0;
+})
+//creer une fonction qui play une music 
 
-audioOn.addEventListener('click', ()=>{
-    audioOn.classList.add('hidden'); 
-    audioOff.classList.remove('hidden'); 
+//--------------------hidden au clic commencer --------------------------
+// variable du main et header 
+const header = document.getElementById('header')
+const main = document.getElementById('main')
+//ecouter l element bouton start
+start.addEventListener('click', () => {
+    //je  cache le header (menu principal)
+    header.classList.add('hidden');
+    //je fais apparaitre le main (l animation)
+    main.classList.remove('hidden');
+
 })
-audioOff.addEventListener('click', ()=>{
-    audioOff.classList.add('hidden'); 
-    audioOn.classList.remove('hidden'); 
+
+//------------------------btn close-------------------------------------
+//variable du bouton close
+const btnClose = document.querySelector('.btnClose');
+
+//Ecoute la bouton close 
+btnClose.addEventListener('click', () => {
+    location.reload(); 
+    //je  cache le main (l animation)
+    main.classList.add('hidden');
+    //je fais apparaitre le header (menu principal)
+    header.classList.remove('hidden');
 })
+
+//-----------------------------Select----------------------------------
+// envoyer tout les valeur du select dans la let "temps"
+
+//variable du compteur
+const select = document.querySelector("#compteur"); 
+
+//ecouter l element bouton start
+let id = null; 
+    start.addEventListener('click', () => {
+    //peut aussi s appeler comme ceci :
+        // start.onclick = ()=>{
+    // definir la value du selecteur
+    let selecTime = select.value;  
+    //envois les informations de timer a partir du moment ou l animation apparaît 
+    if ( main.classList.remove('hidden') === main.classList.remove('hidden') ){
+        //appel les valeurs de select 
+    switch (selecTime) {
+        //dans le cas ou la valeur 1 (1 min) est selectionnée
+        case "1":
+            //action faite 
+            //message 'appuyez sur une touche' pour démmarer 
+            timerElement.textContent = "appuyez sur une touche";
+            //demarre si appuye sur une touche 
+            window.addEventListener('keydown', () => {
+            //j'envois la valeur du 'temp' si case 1 séléctionné 
+            temps = 60;
+            // renvoi les valeur a la fontcion miminuerTimer
+            diminuerTimer(selecTime )})
+            // if (play musique si btn son active)
+
+            break;
+        case "2":
+            temps = 120;
+            diminuerTimer(selecTime )
+            break;
+        case "3":
+            temps = 180;
+            diminuerTimer(selecTime )
+            break;
+        case "4":
+            temps = 240;
+            diminuerTimer(selecTime)
+            break;
+        case "5":
+            temps = 300;
+            diminuerTimer(selecTime)
+            break;
+        default:
+            window.alert('veuillez choisir une durée'); 
+            break;
+    }}
+})
+//--------------------------------Timer--------------------------------
+
+//https://www.commentcoder.com/timer-javascript/
+
+// definir la base de depart du timer (test)
+// le temps = a la value de mon select
+let temps = 60;
+//definir la base de depart du timer en ciblant les elements d affichage et ajout de ce timer
+const timerElement = document.getElementById('timer'); 
+//fonction pour diminuer l affichage de -1 par -1
+    function diminuerTimer() {
+
+        // diminuer le timer par interval de 1 seconde
+        setInterval(() =>{
+    // definir les minutes et les seconde
+
+    let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
+
+    //ajoutes les 0 au nombre < à la dizaine
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    secondes = secondes < 10 ? "0" + secondes : secondes;
+
+    //ajouter le text à afficher (ici des nombres)
+    timerElement.textContent = minutes + ":" + secondes; 
+
+    // //action des -1 
+    // temps --; 
+
+    //arrêter de timer une fois arrivé a zéro 
+    temps = temps <= 0 ? 0 : temps - 1;  
+
+    },1000 ); }
+
+    //---------------------------temps d animation ------------------------------------
+
+// selection de la div .style.animation = 10s linear  infinite  alternate  bouche
+// localStorage.reload
