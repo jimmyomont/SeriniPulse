c9d18d0957fdb10ecc2e2c68bb2f6ff1d8eb85b5
jimmy  (°O°) <117820706+jimmyomont@users.noreply.github.com>
2023-08-12 10:16:52 +0200

    UpDate README

--- README.md
+++ README.md
@@ -1,3 +1,128 @@
-# voir sur le site : 
+# SereniPulse: Child's Peaceful Path
 
-# [Coherence_Cardiaque_Planet](https://coherence-cardiaque-nu.vercel.app/#)
+## [Lien vers SereniPulse](https://serenipulse.vercel.app/)
+
+L'application de cohérence cardiaque conçue pour les plus jeunes (et pas que ..) propose une expérience ludique et apaisante, inspirée par l'esthétique d'un certain jeu vidéo de notre enfance. 
+Les différentes technologies HTML, CSS, JavaScript sont utilisées pour créer une interface interactive et esthétiquement agréable.
+
+----
+
+## Classification detaillée des éléments et des leurs fonctionalités  :
+
+## Timer et Gestion du Temps :
+
+- Élément associé : #timer
+- Fonctionnalité : Affiche le temps restant de la session de cohérence cardiaque.
+- Détails techniques :
+    - Utilisation de setInterval pour mettre à jour le timer chaque seconde.
+    - Calcul des minutes et des secondes à partir du temps restant.
+    - Affichage du temps formaté dans l'élément #timer.
+    - Arrêt du timer une fois que le temps atteint zéro.
+
+## Boutons de Lecture/Pause de la Musique :
+
+- Éléments associés : #play, #pause
+- Fonctionnalité : Contrôle la lecture et la pause de la musique d'ambiance.
+- Détails techniques :
+    - Écoute des événements click sur les boutons de lecture et de pause.
+    - Affichage et masquage appropriés des boutons en fonction de l'état de lecture.
+    - Utilisation des méthodes .play() et .pause() pour contrôler la musique.
+
+### Sélection du Rythme de Respiration :
+
+- Élément associé : #respiration
+- Fonctionnalité : Permet à l'utilisateur de choisir un rythme de respiration.
+- Détails techniques :
+    - Écoute de l'événement change sur le sélecteur de rythme.
+    - Utilisation d'une structure switch pour appliquer des classes CSS en fonction de la sélection.
+
+### Sélection de la Durée de la Session :
+
+- Élément associé : #compteur
+- Fonctionnalité : Permet à l'utilisateur de choisir la durée de la session.
+- Détails techniques :
+    - Écoute de l'événement click sur le bouton "commencer".
+    - Récupération de la valeur sélectionnée.
+    - Utilisation d'une structure switch pour définir la durée de la session en fonction de la sélection.
+
+### Bouton "Commencer" et Gestion de l'Animation :
+
+- Élément associé : #start
+- Fonctionnalité : Lance la session de cohérence cardiaque et active l'animation.
+- Détails techniques :
+    - Écoute de l'événement click sur le bouton "commencer".
+    - Masquage du menu principal et affichage de l'animation.
+    - Activation du timer et des animations de respiration.
+
+### Bouton de Fermeture de la Session :
+
+- Élément associé : .btnClose
+- Fonctionnalité : Permet de fermer la session en cours et de revenir au menu principal.
+- Détails techniques :
+    - Écoute de l'événement click sur le bouton de fermeture.
+    - Rechargement de la page pour réinitialiser l'interface.
+
+### Gestion des Éléments Visuels de la Mascotte :
+
+- Éléments associés : Différentes parties de la mascotte (left-cheek, right-cheek, left-eye, etc.)
+- Fonctionnalité : Anime visuellement la mascotte pour refléter la respiration et le rythme choisis.
+- Détails techniques :
+    - Ajout et suppression de classes CSS pour appliquer les animations et les transformations.
+    - Utilisation de classes spécifiques pour représenter les différents rythmes de respiration.
+
+### Gestion des Événements Audio (On/Off) :
+
+- Éléments associés : #iconOn, #iconOff
+- Fonctionnalité : Active ou désactive la lecture de la musique d'ambiance.
+- Détails techniques :
+    - Écoute des événements click sur les boutons d'activation et de désactivation.
+    - Utilisation de méthodes pour lire et mettre en pause l'audio.
+
+### Interaction avec les Sélections et Démarrage de la Session :
+
+- Détails techniques :
+- Écoute des événements click ou change sur les différents éléments de sélection.
+- Collecte des valeurs sélectionnées pour définir la durée de la session et le rythme de respiration.
+- Démarrage du timer et des animations lorsque la session est lancée.
+
+----
+## Fonctionnalités 
+
+### Interface Visuelle:
+
+- Conçue avec des couleurs roses et des éléments graphiques qui rappellent le fameux jeu vidéo.
+- Des éléments visuels comme les joues et les yeux ajoutent une touche amusante à l'expérience.
+
+### Musique d'ambiance et Commandes Audio :
+
+- L'utilisateur peut choisi d'activer la musique d'ambiance en appuyant sur le bouton de lecture.
+- Des icônes stylisées représentent les commandes audio, avec son changement visuels lorsque la musique est activée ou désactivée.
+
+### Sélection du Rythme de Respiration :
+
+- L'utilisateur peut choisir parmi différents rythmes de respiration (6/4, 5/5, 4/6, 3/7) pour la pratique de la cohérence cardiaque.
+- L'animation visuelle du rythme de respiration est représentée par la bouche de l'élément graphique, suivant le rythme choisi.
+
+### Durée de la Session :
+
+- L'utilisateur peut sélectionner la durée de la session de cohérence cardiaque parmi différentes options (1 à 5 minutes).
+- Le timer est affiché à l'écran, indiquant le temps restant dans la session.
+
+
+### Démarrage de la Session :
+
+En cliquant sur le bouton "commencer", l'utilisateur lance la session de cohérence cardiaque.
+L'animation visuelle se dévoile, avec une mascotte qui suit le rythme respiratoire et le timer.
+
+### Bouton de Fermeture :
+
+Un bouton de fermeture permet à l'utilisateur de revenir au menu principal à tout moment.
+
+### Interaction et Jeu :
+
+L'application propose une expérience interactive et engageante, incitant l'utilisateur à suivre le rythme de respiration de la mascotte.
+Les éléments visuels réagissent au rythme respiratoire, créant une atmosphère de jeu amusante et relaxante.
+
+### Intégration Audio :
+
+L'application intègre des éléments audio pour la musique d'ambiance et les sons d'interaction.
--- script.js
+++ script.js
@@ -18,7 +18,7 @@ audioOn.addEventListener('click', () => {
     //fait apparaitre le logo off 
     audioOff.classList.remove('hidden');
     //lancer la piste audio 
-    audio.play(); 
+    audio.play();
 })
 //ecoute le bouton off
 audioOff.addEventListener('click', () => {
@@ -36,16 +36,16 @@ audioOff.addEventListener('click', () => {
 const play = document.getElementById('play');
 const pause = document.getElementById('pause');
 
-pause.addEventListener('click', ()=>{
+pause.addEventListener('click', () => {
     play.classList.remove('hidden');
     pause.classList.add('hidden');
     audio.pause();
 })
-play.addEventListener('click', ()=>{
+play.addEventListener('click', () => {
     play.classList.add('hidden');
     pause.classList.remove('hidden');
     audio.play()
-    audio.currentTime = 0 ; 
+    audio.currentTime = 0;
 })
 
 //--------------------hidden au clic commencer --------------------------
@@ -67,7 +67,7 @@ const btnClose = document.querySelector('.btnClose');
 
 //Ecoute la bouton close 
 btnClose.addEventListener('click', () => {
-    location.reload(); 
+    location.reload();
     //je  cache le main (l animation)
     main.classList.add('hidden');
     //je fais apparaitre le header (menu principal)
@@ -78,80 +78,82 @@ btnClose.addEventListener('click', () => {
 // envoyer tout les valeur du select dans la let "temps"
 
 //variable du compteur
-const select = document.querySelector("#compteur"); 
+const select = document.querySelector("#compteur");
 
 //ecouter l element bouton start
-let id = null; 
-    start.addEventListener('click', () => {
+let id = null;
+start.addEventListener('click', () => {
     //peut aussi s appeler comme ceci :
-        // start.onclick = ()=>{
+    // start.onclick = ()=>{
     // definir la value du selecteur
-    let selecTime = select.value;  
+    let selecTime = select.value;
     //envois les informations de timer a partir du moment ou l animation apparaît 
-    if ( main.classList.remove('hidden') === main.classList.remove('hidden') ){
+    if (main.classList.remove('hidden') === main.classList.remove('hidden')) {
         //appel les valeurs de select 
-    switch (selecTime) {
-        //dans le cas ou la valeur 1 (1 min) est selectionnée
-        case "1":
-            //j'envois la valeur du 'temp' si case 1 séléctionné 
-            temps = 60;
-            // renvoi les valeur a la fontcion miminuerTimer
-            diminuerTimer(selecTime );
-            // if (play musique si btn son active)
-            break;
-        case "2":
-            temps = 120;
-            diminuerTimer(selecTime )
-            break;
-        case "3":
-            temps = 180;
-            diminuerTimer(selecTime )
-            break;
-        case "4":
-            temps = 240;
-            diminuerTimer(selecTime)
-            break;
-        case "5":
-            temps = 300;
-            diminuerTimer(selecTime)
-            break;
-        default:
-            window.alert('veuillez choisir une durée'); 
-            location.reload(); 
-            break;
-    }}
+        switch (selecTime) {
+            //dans le cas ou la valeur 1 (1 min) est selectionnée
+            case "1":
+                //j'envois la valeur du 'temp' si case 1 séléctionné 
+                temps = 60;
+                // renvoi les valeur a la fontcion miminuerTimer
+                diminuerTimer(selecTime);
+                // if (play musique si btn son active)
+                break;
+            case "2":
+                temps = 120;
+                diminuerTimer(selecTime)
+                break;
+            case "3":
+                temps = 180;
+                diminuerTimer(selecTime)
+                break;
+            case "4":
+                temps = 240;
+                diminuerTimer(selecTime)
+                break;
+            case "5":
+                temps = 300;
+                diminuerTimer(selecTime)
+                break;
+            default:
+                window.alert('veuillez choisir une durée');
+                location.reload();
+                break;
+        }
+    }
 })
 
 //--------------------------------rythme de réspiration-------------------------
-const respiration = document.getElementById ('respiration'); 
+const respiration = document.getElementById('respiration');
 const bouche = document.querySelector('.mouth')
-start.addEventListener ('click', ()=>{ 
-        let respirationValue = respiration.value; 
-    if ( main.classList.remove('hidden') === main.classList.remove('hidden') ){
+start.addEventListener('click', () => {
+    let respirationValue = respiration.value;
+    if (main.classList.remove('hidden') === main.classList.remove('hidden')) {
 
-const rythme = document.getElementById('rythme')
-    switch (respirationValue){
-        case "1": 
-        rythme.classList.add('rythme64');
-        break;
+        const rythme = document.getElementById('rythme')
+        switch (respirationValue) {
+            case "1":
+                rythme.classList.add('rythme64');
+                break;
 
-        case "2": 
-        rythme.classList.add('rythme55');
-        break;
+            case "2":
+                rythme.classList.add('rythme55');
+                break;
 
-        case "3": 
-        rythme.classList.add('rythme46');
-        break;
+            case "3":
+                rythme.classList.add('rythme46');
+                break;
 
-        case "4": 
-        rythme.classList.add('rythme37');
-        break;
+            case "4":
+                rythme.classList.add('rythme37');
+                break;
 
-    default:
-        window.alert('veuillez choisir un rythme'); 
-        location.reload(); 
-        break;
-    }}
+            default:
+                window.alert('veuillez choisir un rythme');
+                location.reload();
+                break;
+        }
+    }
 });
 
 
@@ -161,42 +163,31 @@ const rythme = document.getElementById('rythme')
 //https://www.commentcoder.com/timer-javascript/
 
 // definir la base de depart du timer (test)
-// le temps = a la value de mon select
 let temps = 60;
 //definir la base de depart du timer en ciblant les elements d affichage et ajout de ce timer
-const timerElement = document.getElementById('timer'); 
+const timerElement = document.getElementById('timer');
 //fonction pour diminuer l affichage de -1 par -1
-    function diminuerTimer() {
+function diminuerTimer() {
 
-        // diminuer le timer par interval de 1 seconde
-        setInterval(() =>{
-    // definir les minutes et les seconde
+    // diminuer le timer par interval de 1 seconde
+    setInterval(() => {
+        // definir les minutes et les seconde
 
-    let minutes = parseInt(temps / 60, 10)
-    let secondes = parseInt(temps % 60, 10)
+        let minutes = parseInt(temps / 60, 10)
+        let secondes = parseInt(temps % 60, 10)
 
-    //ajoutes les 0 au nombre < à la dizaine
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    secondes = secondes < 10 ? "0" + secondes : secondes;
+        //ajoutes les 0 au nombre < à la dizaine
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        secondes = secondes < 10 ? "0" + secondes : secondes;
 
-    //ajouter le text à afficher (ici des nombres)
-    timerElement.textContent = minutes + ":" + secondes; 
+        //ajouter le text à afficher (ici des nombres)
+        timerElement.textContent = minutes + ":" + secondes;
 
-    // //action des -1 
-    // temps --; 
+        // //action des -1 
+        // temps --; 
 
-    //arrêter de timer une fois arrivé a zéro 
-    temps = temps <= 0 ? 0 : temps - 1;  
+        //arrêter de timer une fois arrivé a zéro 
+        temps = temps <= 0 ? 0 : temps - 1;
 
-    },1000 ); }
-
-//------------------------compteur de visite------------------------------
-const counter = document.getElementById('counter')
-const updateCounter = async () =>{
-    const data = await fetch( "https://api.countapi.xyz/hit/counter_Coeherence_Cardiaque/visits");
-    const count = await data.json()
-    counter.textContent = count.value
-
+    }, 1000);
 }
-updateCounter()
-console.log(counter);
